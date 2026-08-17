2398145f963a91dab70a2125137a37984d5e82c8
Yash <60492309+YashM7@users.noreply.github.com>
2026-08-17 04:15:43 -0500

    Add transactional order processing with PostgreSQL

--- order-worker/src/main/java/com/rabbitmq/order_worker/consumer/OrderConsumer.java
+++ order-worker/src/main/java/com/rabbitmq/order_worker/consumer/OrderConsumer.java
@@ -1,12 +1,19 @@
 package com.rabbitmq.order_worker.consumer;
 
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.order_worker.service.OrderProcessor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.core.Message;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderConsumer {
+
+    private final OrderProcessor orderProcessor;
+
+    public OrderConsumer(OrderProcessor orderProcessor) {
+        this.orderProcessor = orderProcessor;
+    }
 
     @RabbitListener(queues = "order.queue")
     public void consumeOrder(Message message, Channel channel) throws Exception {
@@ -15,10 +22,17 @@ public class OrderConsumer {
 
         System.out.println("Received order: " + order);
 
-        channel.basicNack(
+        orderProcessor.process(order);
+
+        channel.basicAck(
                 message.getMessageProperties().getDeliveryTag(),
-                false,
                 false
         );
+
+//        channel.basicNack(
+//                message.getMessageProperties().getDeliveryTag(),
+//                false,
+//                false
+//        );
     }
 }
